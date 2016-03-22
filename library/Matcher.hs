7c85b3147cf8198c0013cdd85ef77cb70e84cd6f
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-03-22 11:39:18 +0300

    Init

---
+++ library/Matcher.hs
@@ -0,0 +1,98 @@
+module Matcher
+(
+  Matcher,
+  run,
+  equals,
+  satisfies,
+  converts,
+)
+where
+
+import Matcher.Prelude
+import qualified Success.Pure
+
+
+-- |
+-- Converts the matcher into a conversion function,
+-- which results in either a successful result or a failure.
+run :: Matcher a b -> a -> Either Text b
+run (Matcher (ReaderT successFn)) input =
+  either (Left . fromMaybe "") Right $
+  Success.Pure.asEither $
+  successFn input
+
+
+-- |
+-- A composable abstraction for checking or converting a context value.
+newtype Matcher a b =
+  Matcher (ReaderT a (Success Text) b)
+  deriving (Functor, Applicative, Alternative, Monad, MonadPlus)
+
+instance Profunctor Matcher where
+  {-# INLINE lmap #-}
+  lmap fn (Matcher (ReaderT successFn)) =
+    Matcher (ReaderT (successFn . fn))
+  {-# INLINE rmap #-}
+  rmap =
+    fmap
+
+instance Category Matcher where
+  {-# INLINE id #-}
+  id =
+    Matcher $
+    ReaderT $
+    Success.Pure.success
+  {-# INLINE (.) #-}
+  (.) (Matcher (ReaderT successFn2)) (Matcher (ReaderT successFn1)) =
+    Matcher $
+    ReaderT $
+    successFn1 >=> successFn2
+
+instance Arrow Matcher where
+  {-# INLINE arr #-}
+  arr f =
+    Matcher $
+    ReaderT $
+    Success.Pure.success . f
+  {-# INLINABLE first #-}
+  first (Matcher (ReaderT successFn)) =
+    Matcher $
+    ReaderT $
+    \(a, b) ->
+      fmap (\a -> (a, b)) $
+      successFn a
+
+
+-- |
+-- Tests the matched value on equality with the provided value.
+{-# INLINABLE equals #-}
+equals :: Eq a => a -> Matcher a ()
+equals reference =
+  Matcher $
+  ReaderT $
+  \input ->
+    if input == reference
+      then Success.Pure.success ()
+      else Success.Pure.failure "The input doesn't equal the expected value"
+
+-- |
+-- Checks whether the matched value satisfies the provided predicate.
+{-# INLINABLE satisfies #-}
+satisfies :: (a -> Bool) -> Matcher a ()
+satisfies predicate =
+  Matcher $
+  ReaderT $
+  \input ->
+    if predicate input
+      then Success.Pure.success ()
+      else Success.Pure.failure "The input doesn't satisfy the predicate"
+
+-- |
+-- Tries to convert the matched value to an output value,
+-- with 'Either' encoding the success or failure of the conversion.
+{-# INLINABLE converts #-}
+converts :: (a -> Either Text b) -> Matcher a b
+converts match =
+  Matcher $
+  ReaderT $
+  either Success.Pure.failure Success.Pure.success . match
---
+++ library/Matcher/Prelude.hs
@@ -0,0 +1,32 @@
+module Matcher.Prelude 
+(
+  module Exports,
+)
+where
+
+-- base-prelude
+-------------------------
+import BasePrelude as Exports
+
+-- transformers
+-------------------------
+import Control.Monad.IO.Class as Exports
+import Control.Monad.Trans.Class as Exports
+import Control.Monad.Trans.Cont as Exports hiding (shift, callCC)
+import Control.Monad.Trans.Except as Exports (ExceptT(ExceptT), Except, except, runExcept, runExceptT, mapExcept, mapExceptT, withExcept, withExceptT)
+import Control.Monad.Trans.Maybe as Exports
+import Control.Monad.Trans.Reader as Exports (Reader, runReader, mapReader, withReader, ReaderT(ReaderT), runReaderT, mapReaderT, withReaderT)
+import Control.Monad.Trans.State.Strict as Exports (State, runState, evalState, execState, mapState, withState, StateT(StateT), runStateT, evalStateT, execStateT, mapStateT, withStateT)
+import Control.Monad.Trans.Writer.Strict as Exports (Writer, runWriter, execWriter, mapWriter, WriterT(..), execWriterT, mapWriterT)
+
+-- profunctors
+-------------------------
+import Data.Profunctor.Unsafe as Exports
+
+-- success
+-------------------------
+import Success.Pure as Exports (Success)
+
+-- text
+-------------------------
+import Data.Text as Exports (Text)
